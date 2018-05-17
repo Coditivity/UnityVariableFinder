b8fe6904b586d9dbc2aa6b7e20423091097fd5da
Ajith <ajith.g2000@gmail.com>
2018-05-17 08:01:44 +0530

    >>Initial commit

---
+++ Assets/Scripts/Editor/VariableSearchWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class VariableSearchWindow : EditorWindow {
+
+
+    static MonoBehaviour _monoBehaviour = null;
+    static SerializedObject _serializedObject = null;
+
+    [MenuItem("CONTEXT/MonoBehaviour/Find Variable")]    
+    private static void SearchWidow(MenuCommand menuCommand)
+    {
+
+        _monoBehaviour = menuCommand.context as MonoBehaviour;
+        _serializedObject = new UnityEditor.SerializedObject(_monoBehaviour);
+        GetWindow(typeof(VariableSearchWindow));
+    }
+
+
+    string variableName = null;
+    string variableCount = "";
+    private void OnGUI()
+    {
+        
+        string newVariableName = EditorGUILayout.TextField("Variable name:", variableName);
+        FieldInfo[] fields = null;
+        if(string.Compare(newVariableName, variableName) != 0)
+        {
+            Type type = _monoBehaviour.GetType();
+            fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            variableName = newVariableName;
+            variableCount = fields[0].Name;// .Length.ToString();
+            
+        }
+        
+       // if(fields!=null)
+        {
+            //if (fields.Length > 0)
+            {
+                GUIContent content = new GUIContent("Name: ");
+                EditorGUILayout.PropertyField(_serializedObject.FindProperty("potato"), content);
+                _serializedObject.ApplyModifiedProperties();
+            }
+        }
+       
+
+
+
+        
+    }
+
+    private void OnDestroy()
+    {
+        _monoBehaviour = null;
+        _serializedObject = null;
+    }
+}
